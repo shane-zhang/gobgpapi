3c772c91165cc6c8750c0992eacb4d272d1d68ec
Balakrishnan Chandrasekaran <balakrishnan.c@gmail.com>
2019-03-05 14:24:42 +0000

    Init commit

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import render_template
+import connexion
+
+app = connexion.App(__name__, specigication_dir = './')
+
+app.add_api('swagger.yml')
+
+
+@app.route("/")
+
+def home()
+    return render_template("home.html")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug = True)
