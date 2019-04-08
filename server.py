c9f886915894d7b33d4db8f840f8f86208906181
shane <zx0319@gmail.com>
2019-04-08 20:50:46 +0000

    Add the basic auth to the API framework

--- server.py
+++ server.py
@@ -1,6 +1,13 @@
 from flask import render_template
 import connexion
 
+def basic_auth(username, password, required_scopes=None):
+    if username == 'admin' and password == 'secret':
+        return {'sub': 'admin'}
+
+    # optional: raise exception for custom error response
+    return None
+
 app = connexion.App(__name__, specification_dir = './')
 
 app.add_api('swagger.yml')
